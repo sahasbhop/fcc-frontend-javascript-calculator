a9c6540d7270294c4351c32e2b01bc42829b3de8
Terng Sahasbhop <sahasbhop.suvadhanabhakdi@scbtechx.io>
2022-03-14 15:10:00 +0700

    Cleanup project and init main UI layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [inputText, setInputText] = useState('')
+
+    const onEditorChange = (event) => {
+        let inputText = event.target.value;
+        setInputText(inputText)
+    }
+    return (
+        <div id="app" className="App">
+            <div id="title">Markdown Previewer</div>
+            <div id="body">
+                <div id="editor-container">
+                    Editor
+                    <textarea id="editor" onChange={onEditorChange}/>
+                </div>
+                <div id="preview-container">
+                    Preview
+                    <div id="preview">{inputText}</div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
